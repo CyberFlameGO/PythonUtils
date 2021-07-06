1659cb494c2ce1a55a4ddaf342b797186bb0d883
CyberFlame <cyberflameu@gmail.com>
2021-07-06 14:07:20 +1200

    sandobx

--- sandbox.py
+++ sandbox.py
@@ -11,3 +11,28 @@ for line in stdin_fileno:
         exit(0)
     else:
         print('Message from sys.stdin: ---> {} <---'.format(line))
+
+stdout_fileno = sys.stdout
+
+sample_input = ['Hi', 'Hello', 'exit']
+
+for ip in sample_input:
+    # Prints to stdout
+    stdout_fileno.write(ip + '\n')
+
+import sys
+
+stdout_fileno = sys.stdout
+stderr_fileno = sys.stderr
+
+sample_input = ['Hi', 'Hello', 'exit']
+
+for ip in sample_input:
+    # Prints to stdout
+    stdout_fileno.write(ip + '\n')
+    # Tries to add an Integer with string. Raises an exception
+    try:
+        ip = ip + 100
+    # Catch all exceptions
+    except:
+        stderr_fileno.write('Exception Occurred!\n')
